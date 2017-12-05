aaa221177c0252ad9846b86b6a2a15e0a8f1aaa0
jakmagaud <jakmagaud@college.harvard.edu>
2017-12-05 15:42:41 -0500

    more comprehensive typo generation

--- jank.py
+++ jank.py
@@ -1,15 +1,47 @@
 import sys
 
+close_letters = {
+    'q': {'w', 'a'},
+    'a': {'z', 's', 'q'},
+    'z': {'a', 'x'},
+    'w': {'q', 'e'},
+    's': {'a', 'd'},
+    'x': {'z', 'c'},
+    'e': {'w', 'r'},
+    'd': {'s', 'f'},
+    'c': {'x', 'v'},
+    'r': {'e', 't'},
+    'f': {'d', 'g'},
+    'v': {'c', 'b'},
+    't': {'r', 'y'},
+    'g': {'f', 'h'},
+    'b': {'v', 'n'},
+    'y': {'t', 'u'},
+    'h': {'g', 'j'},
+    'n': {'b', 'm'},
+    'u': {'y', 'i'},
+    'j': {'h', 'k'},
+    'm': {'b', 'm'},
+    'i': {'u', 'o'},
+    'k': {'j', 'l'},
+    'o': {'i', 'p', 'l'},
+    'l': {'k', 'o'},
+    'p': {'o'}
+}
+
 def insert_char(s, c, i):
     assert len(s) > 0 and len(c) == 1 and i in range(0, len(s) + 1)
     return s[:i] + c + s[i:]
 
-def replace_char(s, i, j):
+def replace_char(s, c, i, j):
     assert len(s) > 0 and i in range(0, len(s)) and j in range(0, len(s))
     l = list(s)
-    tmp = l[i]
-    l[i] = l[j]
-    l[j] = tmp
+    if c == None:
+        tmp = l[i]
+        l[i] = l[j]
+        l[j] = tmp
+    else:
+        l[i] = c
     return ''.join(l)
 
 def delete_char(s, i):
@@ -18,17 +50,26 @@ def delete_char(s, i):
 
 def typo_generator(s):
     results = set()
+    results.add(s.replace('-',''))
 
     for i, char in enumerate(s):
+        fat_finger = close_letters.get(char)
+        if fat_finger != None:
+            for letter in fat_finger:
+                results.add(insert_char(s, letter, i))
+                results.add(insert_char(s, letter, i+1))
+                results.add(replace_char(s, letter, i, i))
+
         results.add(delete_char(s, i))
         results.add(insert_char(s, '-', i))
+
         for j, _ in enumerate(s):
             results.add(insert_char(s, char, j))
-            results.add(replace_char(s, i, j))
+            results.add(replace_char(s, None, i, j))
     return results
 
 def test_typos():
-    pack_names = ['reactdom']
+    pack_names = ['react-dom']
     for name in pack_names:
         print(typo_generator(name))
 
@@ -39,9 +80,10 @@ def find_similar_packages(s):
         return[s]
 
 if __name__ == "__main__":
-    """
     pack_name = sys.argv[1]
-    packs = find_similar_packages(pack_name)
+    packs = typo_generator(pack_name)
+    print packs
+    """
     if len(packs) > 1:
         choices = ""
         for index, pack in enumerate(packs):
@@ -52,5 +94,4 @@ if __name__ == "__main__":
     else:
         print("npm install " + pack_name)
         """
-    test_typos()
 
